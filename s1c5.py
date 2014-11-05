5eb19a56228ab083a8a8b6a6d3188fd337099812
sokampdx <sokam@pdx.edu>
2014-11-05 00:10:06 -0800

    completed set1 challenge5

---
+++ s1c5.py
@@ -0,0 +1,14 @@
+#!/bin/python
+
+import util
+
+plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+key = "ICE"
+cipher = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+result = util.asciiToHex(util.repeatKeyXor(key, plaintext))
+print plaintext
+print key
+print result
+print result == cipher
+
--- util.py
+++ util.py
@@ -3,55 +3,6 @@ import base64
 import binascii
 import string
 
-def testSingleXor(searchStr):
-	# frequency alphabet dictionary
-	freqDict = {'a':8.2, 'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7, 
-		 		'f':2.2, 'g':2.0, 'h':6.1, 'i':7.0, 'j':0.2, 
-			    'k':0.8, 'l':4.0, 'm':2.4, 'n':6.7, 'o':7.5, 
-		    	'p':1.9, 'q':0.1, 'r':6.0, 's':6.3, 't':9.1,
-				'u':2.8, 'v':1.0, 'w':2.4, 'x':0.2, 'y':2.0, 'z':0.1}
-	freqSeq = "etaoinsrhdlucmfywgpbvkxqjz"
-	char = string.ascii_letters + string.digits + ' '
-
-	# local dictionary file
-	filename = "/usr/share/dict/words"
-
-	def findXor(searchStr):
-		cipher = hexToAscii(searchStr)
-		length = len(cipher)
-		maxSet = 0, 'a', ''
-
-		for key in char:
-			decipher = xorStrings(cipher, key*length)
-			score = charProfiler(decipher)
-			if maxSet[0] < score:
-				maxSet = score, key, decipher
-			# print maxSet
-		return maxSet
-
-	def wordProfiler(searchStr):
-		dictionary = open(filename).read()
-		score = 0
-		wordList = searchStr.split()
-		dictList = dictionary.split()
-		
-		for word in wordList:
-			for dictWord in dictList:
-				if word == dictWord:
-					score += 1
-		return score
-
-	def charProfiler(searchStr):
-		score = 0
-		for c in searchStr:
-			if c in char:
-				score += 1
-		return float("{0:.3f}".format(float(score)/len(searchStr)))
-
-	# main
-	return findXor(searchStr)
-
-
 def hexToAscii(s):
 	return binascii.unhexlify(s)
 
@@ -73,9 +24,63 @@ def xorStrings(s1,s2):
 		result += chr(ord(c1) ^ ord(c2))
 	return result
 
+def testSingleXor(searchStr):
+	char = string.ascii_letters + string.digits + ' '
+
+	# local dictionary file
+	filename = "/usr/share/dict/words"
+
+	def wordProfiler(searchStr):
+		dictionary = open(filename).read()
+		score = 0
+		wordList = searchStr.split()
+		dictList = dictionary.split()
+		
+		for word in wordList:
+			for dictWord in dictList:
+				if word == dictWord:
+					score += 1
+		return score
+
+	def findXor(searchStr):
+		cipher = hexToAscii(searchStr)
+		length = len(cipher)
+		maxSet = 0, 'a', ''
+
+		for key in char:
+			decipher = xorStrings(cipher, key*length)
+			score = charProfiler(decipher)
+			if maxSet[0] < score:
+				maxSet = score, key, decipher
+			# print maxSet
+		return maxSet
+
+	def charProfiler(searchStr):
+		score = 0
+		for c in searchStr:
+			if c in char:
+				score += 1
+		return float("{0:.3f}".format(float(score)/len(searchStr)))
+
+	# main
+	return findXor(searchStr)
+
+def repeatKeyXor(key, plaintext):
+	keyLen = len(key)
+	length = len(plaintext)
+	xorKey = key*(length//keyLen) + key[:(length % keyLen)]
+	
+	return xorStrings(plaintext, xorKey)
 
 
 
 
-
-
+'''
+	# frequency alphabet dictionary
+	freqDict = {'a':8.2, 'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7, 
+		 		'f':2.2, 'g':2.0, 'h':6.1, 'i':7.0, 'j':0.2, 
+			    'k':0.8, 'l':4.0, 'm':2.4, 'n':6.7, 'o':7.5, 
+		    	'p':1.9, 'q':0.1, 'r':6.0, 's':6.3, 't':9.1,
+				'u':2.8, 'v':1.0, 'w':2.4, 'x':0.2, 'y':2.0, 'z':0.1}
+	freqSeq = "etaoinsrhdlucmfywgpbvkxqjz"
+'''

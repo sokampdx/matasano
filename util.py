8f6747d2fa83cdea514d13027d8f413626246dfa
sokampdx <sokam@pdx.edu>
2014-11-04 22:59:36 -0800

    moved test on xor into util

--- util.py
+++ util.py
@@ -1,6 +1,49 @@
 #!/bin/python
 import base64
 import binascii
+import string
+
+def testSingleXor(searchStr):
+	# frequency alphabet dictionary
+	freqDict = {'a':8.2, 'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7, 
+		 		'f':2.2, 'g':2.0, 'h':6.1, 'i':7.0, 'j':0.2, 
+			    'k':0.8, 'l':4.0, 'm':2.4, 'n':6.7, 'o':7.5, 
+		    	'p':1.9, 'q':0.1, 'r':6.0, 's':6.3, 't':9.1,
+				'u':2.8, 'v':1.0, 'w':2.4, 'x':0.2, 'y':2.0, 'z':0.1}
+	freqSeq = "etaoinsrhdlucmfywgpbvkxqjz"
+	char = string.ascii_letters + string.digits
+
+	# local dictionary file
+	filename = "/usr/share/dict/words"
+
+	def findXor(searchStr):
+		cipher = hexToAscii(searchStr)
+		length = len(cipher)
+		maxSet = 0, 'a', ''
+
+		for key in char:
+			decipher = xorStrings(cipher, key*length)
+			score = wordProfiler(decipher)
+			if maxSet[0] < score:
+				maxSet = score, key, decipher
+			# print maxSet
+		return maxSet[1], maxSet[2]
+
+	def wordProfiler(searchStr):
+		dictionary = open(filename).read()
+		score = 0
+		wordList = searchStr.split()
+		dictList = dictionary.split()
+		
+		for word in wordList:
+			for dictWord in dictList:
+				if word == dictWord:
+					score += 1
+		return score
+
+	# main
+	return findXor(searchStr)
+
 
 def hexToAscii(s):
 	return binascii.unhexlify(s)
@@ -22,3 +65,10 @@ def xorStrings(s1,s2):
 	for c1, c2 in zip(s1, s2):
 		result += chr(ord(c1) ^ ord(c2))
 	return result
+
+
+
+
+
+
+

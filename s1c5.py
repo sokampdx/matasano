b346526c7e07e488f16b4ae14ed1ba4359b1327d
sokampdx <sokam@pdx.edu>
2014-11-07 00:37:32 -0800

    redo set 1 challenge 5 in python3

--- s1c5.py
+++ s1c5.py
@@ -2,11 +2,13 @@
 
 import util
 
-plaintext = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
-key = "ICE"
+plainbyte = b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+keybyte = b"ICE"
 cipher = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 
-result = util.asciiToHex(util.repeatKeyXor(key, plaintext))
+plaintext = plainbyte.decode()
+key = keybyte.decode()
+result = util.byte2hex(util.xor_byte(plainbyte, keybyte))
 print (plaintext)
 print (key)
 print (result)

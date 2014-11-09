7d5106495fe66ab3b5fe9ecf0cb55c2a83854412
sokampdx <sokam@pdx.edu>
2014-11-08 18:38:24 -0800

    complete set1 challenge 6

--- s1c6.py
+++ s1c6.py
@@ -3,40 +3,34 @@
 import util
 import operator
 
-s1 = "this is a test"
-s2 = "wokka wokka!!!"
-b1 = util.asciiTobits(s1)
-b2 = util.asciiTobits(s2)
-print "-------------------beg test1-------------"
-print util.hamming(b1, b2)
-print util.bitsToAscii(b1)
-print util.bitsToAscii(b2)
-print "-------------------end test1-------------"
 
-def guessKeySize(s):
-	maxn = 12
+# input a cipher byte stringg
+def guessKeySize(b):
+	maxSize = 41 
 	keySizeScore = []
-	for n in xrange(2, maxn):
+	for n in range(2, maxSize):
 		score = 0.0
 		x = 2*n
-		repeat = len(s)/n/4
-		for i in range(repeat):
-			str1 = s[(i*x):((i+1)*x)]
-			str2 = s[((i+2)*x):((i+3)*x)]
+		repeat = (len(b)//n)//4
+
+		for i in range(int(repeat)):
+			bstr1 = b[(i*x):((i+1)*x)]
+			bstr2 = b[((i+2)*x):((i+3)*x)]
 			#print n, i, str1, str2
-			bit1 = util.hexTobits(str1)
-			bit2 = util.hexTobits(str2)
+			bit1 = util.byte2bit(bstr1)
+			bit2 = util.byte2bit(bstr2)
 			score += util.hamming(bit1, bit2)
 		keySizeScore.append((n, float(score)/n/repeat))
-
 	return keySizeScore
 
+# sort a list of score
 def sortScore(scorelist):
 	return sorted(scorelist, key=operator.itemgetter(1))
 
+
 def printScore(scorelist):
 	for k in scorelist:
-		print k
+		print (k)
 
 def findMinScore(scorelist):
 	minScore = (1, 999.0)
@@ -46,17 +40,62 @@ def findMinScore(scorelist):
 
 	return minScore
 
-plaintext = "I am trying to break this crypto pal challenge. Let see if I can finish the first set this week. I need to work on over the wire krypton wargame as well to be really good at this stuffs. This is a really hard problem that I need to be able to solve but I am not getting the right result at all. Why is it the lowerest hamming distance is not the correct keysize?"
-testkey = " 3J#>"
-cipher = util.asciiToHex(util.repeatKeyXor(testkey, plaintext))
+def decryptXorBlock(cipher, size):
+	blocks = [None]*size
+	for i in range(size):
+		blocks[i] = cipher[i::size]		
+
+	key = ''
+	for b in blocks:
+		key += util.find_single_byte_xor_of(b)[1]
+
+	return key
 
 
-print "-------------------beg test2-------------"
-result = guessKeySize(cipher)
-printScore(sortScore(result)[:3])
-print "-------------------end test2-------------"
+
+
+######### tests ############
+
+def main():
+	filename = "s1c6.txt"
+	cipher = util.b642byte(open(filename).read().strip('\n'))
+	resultList = guessKeySize(cipher)
+	result = findMinScore(resultList)
+	key = decryptXorBlock(cipher, result[0])
+	plaintext = util.xor_byte(cipher, util.str2byte(key)).decode()
+	print ("-------------------beg test3-------------")
+	#print (util.str2byte(cipher))
+	print ("min score \n", result)
+	#print ("cipher text \n", cipher)
+	print ("key \n", key)
+	print ("plaintext \n", plaintext)
+	print ("-------------------end test3-------------")
+
+def test2():
+	plaintext = b"I am trying to break this crypto pal challenge. Let see if I can finish the first set this week. I need to work on over the wire krypton wargame as well to be really good at this stuffs. This is a really hard problem that I need to be able to solve but I am not getting the right result at all. Why is it the lowerest hamming distance is not the correct keysize?"
+	testkey = b" %d3J#>"
+	cipher = util.xor_byte(plaintext, testkey)
+
+	result = guessKeySize(cipher)
+	print ("-------------------beg test2-------------")
+	printScore(sortScore(result)[:5])
+	print ("-------------------end test2-------------")
+
+def test1():
+	s1 = b"this is a test"
+	s2 = b"wokka wokka!!!"
+	b1 = util.byte2bit(s1)
+	b2 = util.byte2bit(s2)
+	print ("-------------------beg test1-------------")
+	print (util.hamming(b1, b2))
+	print (util.bit2byte(b1).decode())
+	print (util.bit2byte(b2).decode())
+	print ("-------------------end test1-------------")
 
 
 
-
+# main interface
+test1()
+test2()
+main()
 
--- util.py
+++ util.py
@@ -7,6 +7,7 @@ import itertools
 import difflib
 import collections
 import string
+import bitstring
 
 # hex <-> bytes
 def hex2byte(h):
@@ -82,16 +83,39 @@ def freq_profile(text):
 	freq = collections.Counter(text.lower())
 	return "".join(map(lambda x: x[0], freq.most_common()))
 
-# higher score mean better closeness
-freq_str = "etaoinsrhdlucmfywgpbvkxqjz"
-freql = 26 
-
+# higher score mean better match to freqency string
+freq_str = " etaoinsrhdlucmfywgpbvkxqjz"
+freqDict = {'a':8.2, 'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7, 
+	 		'f':2.2, 'g':2.0, 'h':6.1, 'i':7.0, 'j':0.2, 
+		    'k':0.8, 'l':4.0, 'm':2.4, 'n':6.7, 'o':7.5, 
+	    	'p':1.9, 'q':0.1, 'r':6.0, 's':6.3, 't':9.1,
+			'u':2.8, 'v':1.0, 'w':2.4, 'x':0.2, 'y':2.0, 'z':0.1}
+freql = 6
+weight = 3
+'''
+# using sum of the frequency value to return a score
 def string_scoring(text1):
-	return difflib.SequenceMatcher(None, freq_profile(text1)[:freql], freq_str[:freql]).quick_ratio()
+	score = 0.0
+	for c in text1:
+		key = c.lower()
+		print (key)
+		if key in freq_str:
+			score += freqDict[key]
+	return score
+'''
+
+# using sequence matcher to return a score
+# fast with mix result
+def string_scoring(text1):
+	scoreHead = difflib.SequenceMatcher(None, (freq_profile(text1))[:freql], freq_str[:freql]).quick_ratio()
+	scoreTail = difflib.SequenceMatcher(None, (freq_profile(text1))[freql:], freq_str[freql:]).quick_ratio()
+	return scoreHead*weight + scoreTail
 	#seq_matcher = difflib.SequenceMatcher(None, text1, text2)
 	#return seq_matcher.quick_ratio()
 
 '''
+# using levenshtein to return a score
+# fast with mix result
 def string_scoring(text1):
 	return levenshtein(freq_profile(text1)[:freql], freq_str[:freql])	
 '''
@@ -115,9 +139,9 @@ def levenshtein(s1, s2):
 		previous = current
 	return previous[-1]
 
-
-'''	
-# looking at word in dictionary
+'''
+# using word match in dictionary to return a score
+# slowest but most acurate
 filename = b"/usr/share/dict/words"
 def string_scoring(searchStr):
 	dictionary = open(filename).read()
@@ -132,109 +156,23 @@ def string_scoring(searchStr):
 	return score
 '''
 
+# bit utilities
+def byte2bit(b):
+	return bitstring.Bits(bytes=b)
 
-
-'''
-def asciiTobits(s): #
-	return bitstring.Bits(bytes=s)
-
-def bitsToAscii(b): #
+def bit2byte(b):
 	return b.tobytes()
 
-def hexTobits(h): #
-	return asciiTobits(hexToAscii(h))
+def hex2bit(h):
+	return byte2bit(hex2byte(h))
 
-def bitsTohex(h): #
+def bit2hex(b):
 	return b.hex
- 
-def hexToAscii(h): 
-	return binascii.a2b_hex(h)
 
-def asciiToHex(s): 
-	return binascii.b2a_hex(s)
-
-def asciiToBase64(s):
-	return binascii.b2a_base64(s)
-
-def base64ToAscii(s):
-	return binascii.a2b_base64(s)
-
-def xorHexStrings(h1, h2):
-	return asciiToHex(xorStrings(hexToAscii(h1), hexToAscii(h2)))
-
-def xorStrings(s1,s2):
-	result = ''
-	print (s1, s2)
-	for c1, c2 in zip(s1, s2):
-		result += chr(c1 ^ c2)
-	return bytes(result, 'utf-8')
-
-def testSingleXor(searchStr): #
-	char = string.ascii_letters + string.digits + ' '
-	#char = b'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-
-	
-	# local dictionary file
-	filename = b"/usr/share/dict/words"
-
-	def wordProfiler(searchStr):
-		dictionary = open(filename).read()
-		score = 0
-		wordList = searchStr.split()
-		dictList = dictionary.split()
-		
-		for word in wordList:
-			for dictWord in dictList:
-				if word == dictWord:
-					score += 1
-		return score
-
-	def findXor(searchStr):
-		cipher = hexToAscii(searchStr)
-		length = len(cipher)
-		maxSet = 0, b'', b''
-
-		for c in char:
-			print (c)
-			key = bytes(c * length, 'utf-8')
-			decipher = xorStrings(cipher, key)
-			score = charProfiler(decipher)
-			if maxSet[0] < score:
-				maxSet = score, key, decipher
-			# print maxSet
-		return maxSet
-
-	def charProfiler(searchStr):
-		score = 0
-		for c in searchStr:
-			match = chr(c)
-			if c in char:
-				score += 1
-		return float(score)/len(searchStr)
-		#return float("{0:.3f}".format(float(score)/len(searchStr)))
-
-	# main
-	return findXor(searchStr)
-
-def repeatKeyXor(key, plaintext): #
-	keyLen = len(key)
-	length = len(plaintext)
-	xorKey = key*(length//keyLen) + key[:(length % keyLen)]
-	
-	return xorStrings(plaintext, xorKey)
-
-def hamming(b1, b2): #
+# count xor of 2 bitstring
+def hamming(b1, b2):
+	assert(len(b1) == len(b2))
 	return (b1 ^ b2).count(1)
 
-'''
 
 
-'''
-	# frequency alphabet dictionary
-	freqDict = {'a':8.2, 'b':1.5, 'c':2.8, 'd':4.3, 'e':12.7, 
-		 		'f':2.2, 'g':2.0, 'h':6.1, 'i':7.0, 'j':0.2, 
-			    'k':0.8, 'l':4.0, 'm':2.4, 'n':6.7, 'o':7.5, 
-		    	'p':1.9, 'q':0.1, 'r':6.0, 's':6.3, 't':9.1,
-				'u':2.8, 'v':1.0, 'w':2.4, 'x':0.2, 'y':2.0, 'z':0.1}
-	freqStr = "etaoinsrhdlucmfywgpbvkxqjz"
-'''

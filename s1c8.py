21b00023bfa68f1db77aa7f6bceb19910f98531e
sokampdx <sokam@pdx.edu>
2014-11-09 13:10:17 -0800

    completed set 1 challenge 8

---
+++ s1c8.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import util
+
+
+def checkECBmode(cipherhexList):
+	for cipherhex in cipherhexList:
+		blocks = [cipherhex[i:i+32] for i in range(0, len(cipherhex), 32)]
+		if len(blocks) != len(set(blocks)):
+			print ("AES ECB mode cipher")
+			for b in blocks:
+				print (b)	
+
+
+def main():
+	filename = "s1c8.txt"
+	cipherhexList = open(filename).read().split('\n')
+	checkECBmode(cipherhexList)
+
+
+main()
